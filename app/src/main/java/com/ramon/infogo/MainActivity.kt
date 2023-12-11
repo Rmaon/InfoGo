434c955bca9d2cac86f2e6d53ddfed0adf5240e3
Rmaon <ramoncamineroarroyo@gmail.com>
2023-12-11 17:22:20 +0100

    Actualizacion de errores

--- app/src/main/java/com/ramon/infogo/MainActivity.kt
+++ app/src/main/java/com/ramon/infogo/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
@@ -70,6 +71,14 @@ class MainActivity : AppCompatActivity() {
                 // No es necesario implementar este método
             }
         })
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.your_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
+
 
         binding.btnRegister.setOnClickListener {
             showRegisterDialog()
